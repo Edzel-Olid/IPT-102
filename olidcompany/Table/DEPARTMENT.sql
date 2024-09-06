77e87b7eb73b19b2dafc3275db4d64e8eef575d2
Edzel-Olid <152491852+Edzel-Olid@users.noreply.github.com>
2024-09-06 18:30:18 +0800

    Add files via upload

---
+++ olidcompany/Table/DEPARTMENT.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DEPARTMENT]
+(
+    [Dname] Varchar(15) NOT NULL ,
+    Dnumber INT NOT NULL ,
+    Mgr_ssn Char(9) NOT NULL,
+    Mgr_start_date Date ,
+    Constraint [PK_DEPARTMENT_Number] Primary KEy (Dnumber),
+    Constraint [UX_DEPARTMENT_Name] UNIQUE nonclustered (Dname),
+    Constraint [FK_DEPARTMENT_SSN] Foreign Key (Mgr_ssn) References EMPLOYEE(SSN)
+);
+GO
+create nonclustered index [IX_DEPARTMENT_Name] On [DEPARTMENT] ([Dname] ASC)

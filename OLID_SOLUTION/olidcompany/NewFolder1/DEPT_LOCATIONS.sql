671e6e7fd420cb7c20facb9741c26a0f8559d2ca
Gil-Alvior <alvior.giheu22@southernchristiancollege.edu.ph>
2024-10-17 09:11:22 +0800

    ok

--- OLID_SOLUTION/olidcompany/NewFolder1/DEPT_LOCATIONS.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE DEPT_LOCATIONS
-(
-    Dnumber INT NOT NULL,
-    Dlocation VARCHAR(15) NOT NULL,
-    PRIMARY KEY (Dnumber, Dlocation),
-    FOREIGN KEY (Dnumber) REFERENCES DEPARTMENT(Dnumber)
-);
-
